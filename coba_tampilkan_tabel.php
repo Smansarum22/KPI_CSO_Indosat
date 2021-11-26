7c7e07cb42ce4ebf2f83c9ca12ca3ac6fbce29b6
Smansarum22 <57025915+Smansarum22@users.noreply.github.com>
2021-11-26 16:30:41 +0800

    tampilkan header kolom

--- coba_tampilkan_tabel.php
+++ coba_tampilkan_tabel.php
@@ -3,21 +3,30 @@
               <div class="table-responsive">
                   <table class="table table-bordered table-striped table-hover">
                       <thead>
-                          <tr>
-                            <th>nama</td>
-                            <th>mc</th>
-                          </tr>
+                      <?php
+                          $conn=mysqli_connect("localhost", "root", "", "xyz");
+
+                          if (!$conn)
+                          {
+                              die("Connection Failed".mysqli_connect_errno());
+                          }
+
+                          $nama_kolom = mysqli_query($conn,"SHOW COLUMNS FROM tabelexcel2"); // fetch data from database
+                          while($data = mysqli_fetch_array($nama_kolom))
+                            {
+                        ?>
+                        
+                        <th><?php echo $data['Field']; ?></td>
+
+                        <?php
+                            }
+                        ?>
+
                       </thead>
 
                       <tbody>
                         <?php
-                            $conn=mysqli_connect("localhost", "root", "", "xyz");
-
-                            if (!$conn)
-                            {
-                                die("Connection Failed".mysqli_connect_errno());
-                            }
-
+                            
                             $records = mysqli_query($conn,"SELECT * FROM tabelexcel2"); // fetch data from database
                             $tabelnya = mysqli_query($conn,"SHOW COLUMNS FROM tabelexcel2");
                             $jumlah_kolom = mysqli_num_rows($tabelnya);
