45e2695aa6c0b2e865b7332e29f891f67be52dac
NurulUtamiRusli <nurulutami.rusli@gmail.com>
2021-11-27 09:45:48 +0700

    Update InputDataAdmin.php

--- InputDataAdmin.php
+++ InputDataAdmin.php
@@ -204,34 +204,3 @@
 
 </html>
 
-<!-- <?
-$con = new PDO("mysql:host=localhost; dbname=CodeFlix", 'root','');
-if (isset($_POST["submit"])){
-  $str = $_POST["search"];
-  $sth = $con->prepare("SELECT * FROM `seacrh` WHERE Name = `$str`");
-
-  $sth->setFetchMode((PDO:: FETCH_OBJ));
-  $sth-> execute();
-
-  if($row = $sth-> fetch())
-  {
-    ?>
-    <br><br><br>
-    <table>
-    <tr>
-      <th>Name</th>
-      <th>Description</th>
-    </tr>
-    <tr>
-    <td><?php echo $row->Name; ?></td>
-    <td><?php echo $row->Description;?></td>
-    </tr>
-    </table>
-<?php
-  }
-
-  else{
-    echo "Name Does not exist";
-  }
-}
-?> -->
