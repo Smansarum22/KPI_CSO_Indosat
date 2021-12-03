8e607a74b14c5e9e7676176d42538a8bc7bb0782
NurulUtamiRusli <nurulutami.rusli@gmail.com>
2021-12-03 14:42:39 +0700

    navbar cso

--- homescreenCSO.php
+++ homescreenCSO.php
@@ -64,9 +64,9 @@
           <!-- <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">Input Data</a>
           </li> -->
-          <li class="nav-item">
+          <!-- <li class="nav-item">
             <a class="nav-link" href='InputDataAdmin.php'>Summary</a>
-          </li>
+          </li> -->
           <li class="nav-item">
             <a class="nav-link" href='summary.html'>Detail</a>
           </li>
